1a026fe276f4a0d6ff9017d068dca6c31f57abbd
tiagobahiense <tgbahiense@gmail.com>
2024-02-14 12:03:00 -0300

    Implementação de js

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
-let textoRecebido = []
+function criptografar(){
+    var textoRecebido = document.getElementById("texto").value;
+    var textoCriptografado = criptografarTexto(textoRecebido);
+    document.getElementById("imagem-lupa").innerText = textoCriptografado;
+}
 
-function recebeTexto(tag, texto){
-    let campo = document.querySelector(tag);
-    campo.innerHTML = texto;
+function criptografarTexto (texto){
+    var textoCriptografado = "";
+    for (var i = 0; i < texto.length; i ++) {
+        var caracter = texto.charAt(i).toLowerCase();
+        switch (caracter) {
+            case 'e':
+                textoCriptografado += "enter";
+                break;
+            case 'i':
+                 textoCriptografado += "imes";
+                break;
+            case 'a':
+                textoCriptografado += "ai";
+                break;
+            case 'o':
+                textoCriptografado += "ober";
+                break;
+            case 'u':
+                textoCriptografado += "ufat";
+                break;
+            default:
+                textoCriptografado += caracter;
+        }
+    }
+    return textoCriptografado;
 }
